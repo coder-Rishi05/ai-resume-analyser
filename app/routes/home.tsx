d12bdc4dfd0d882269b7330727501c921a055026
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-19 19:47:13 +0530

    completed setup and started Home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume App" },
+    { name: "description", content: "Smart Feedback for you dream Job." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+
+  </main>
+  
+  ;
 }
