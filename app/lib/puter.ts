00de3df010484c5651358b690078c7f228769394
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-22 13:47:11 +0530

    setup  PDF to img conversion and AI feedback

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-sonnet-4" }
+      { model: "claude3-7-sonnet" }
     ) as Promise<AIResponse | undefined>;
   };
 
